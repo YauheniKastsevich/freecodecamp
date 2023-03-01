dee40a12f5078dd1631578c06e42707bc942cbb9
YauheniKastsevich <evgecha123@gmail.com>
2023-03-01 20:24:28 +0300

    13

---
+++ task12.js
@@ -0,0 +1,85 @@
+const difference = 45 - 33;
+//
+const product = 8 * 10;
+//
+const quotient = 66 / 33;
+//
+let myVar = 87;
+
+// Only change code below this line
+myVar++;
+//
+let mVar = 11;
+
+// Only change code below this line
+mVar--;
+//
+const ourDecimal = 5.7;
+
+// Only change code below this line
+let myDecimal = 5.7;
+//
+const producte = 2.0 * 2.5;
+//
+const quotiente = 4.4 / 2.0; // Change this line
+//
+const remainder = 11%3;
+//
+
+let a = 3;
+let b = 17;
+let c = 12;
+
+// Only change code below this line
+a += 12;
+b += 9;
+c += 7;
+
+//
+/*let a = 11;
+let b = 9;
+let c = 3;
+
+// Only change code below this line
+a -= 6;
+b -= 15;
+c -= 1;*/
+//
+/*let a = 5;
+let b = 12;
+let c = 4.6;
+
+// Only change code below this line
+a *= 5;
+b *= 3;
+c *= 10;*/
+//
+
+const myStr = "I am a \"double quoted\" string inside \"double quotes\"."
+; 
+// Change this line
+//
+
+/*const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';*/
+//
+
+/*Code	Output
+\'	single quote
+\"	double quote
+\\	backslash
+\n	newline
+\t	tab
+\r	carriage return
+\b	word boundary
+\f	form feed*/
+
+//
+/*const myStr = "FirstLine\n\t\\SecondLine\nThirdLine"; */
+// Change this line
+//
+/*const myStr = "This is the start. " + "This is the end."; */
+
+// Change this line
+//
+
+
