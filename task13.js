23cc5896bf7b631b8a8d566485f8c28fc5c2ecaf
YauheniKastsevich <evgecha123@gmail.com>
2023-03-02 21:17:04 +0300

    15

---
+++ task13.js
@@ -0,0 +1,61 @@
+let myStr = "This is the first sentence. "
+myStr += "This is the second sentence.";
+//
+// Only change code below this line
+const myName = "Eugene";
+const myStre = "My name is " + myName + "and I am well!";
+//
+const someAdjective = "cool";
+let myStr1 = "Learning to code is ";
+myStr1 += someAdjective;
+//
+// Setup
+let lastNameLength = 0;
+const lastName = "Lovelace";
+
+// Only change code below this line
+lastNameLength = lastName.length;
+//
+
+// Setup
+let myStr2 = "Jello World";
+
+// Only change code below this line
+myStr2 = "Hello World"; // Change this line
+// Only change code above this line
+//
+// Setup
+const lastName1 = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName1[2]; // Change this line
+//
+// Setup
+const lastName2 = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName2[lastName2.length - 1]; // Change this line
+//
+// Setup
+const lastName3 = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName3[lastName.length-3]; // Change this line
+//
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks = myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb; // Change this line
+// Only change code above this line
+//
+// Only change code below this line
+const myArray = ["coconut", 1];
+//
+const myArray1 = [[1],[2],["coconut"]];
+//
+const myArray2 = [50, 60, 70];
+let myData = myArray2[0];
+//
